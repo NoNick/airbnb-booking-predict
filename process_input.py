16f3a7b4b247028b0c31310e5a9849763816bbff
NoNick <aleksandrs634@gmail.com>
2019-01-31 17:01:38 +0300

    age-gender by country features addedd

--- process_input.py
+++ process_input.py
@@ -1,4 +1,6 @@
 import math
+import re
+
 import pandas as pd
 import numpy as np
 from pandas._libs.index import datetime
@@ -30,6 +32,7 @@ def normalizeData(data):
     data['signup_method'] = data['signup_method'].map(signupMapping)
 
     data['language'] = mapStrToOrdinals(data['language'])
+    data['language_copy'] = mapStrToOrdinals(data['language_copy'])
 
     data['language'] = mapStrToOrdinals(data['language'])
     data['affiliate_channel'] = mapStrToOrdinals(data['affiliate_channel'])
@@ -85,12 +88,62 @@ def getSecsElapsedByUserId():
     return sessions.groupby('user_id').agg(lambda x: x['secs_elapsed'].sum())
 
 
+def getAgeGenderBuckets():
+    buckets = pd.read_csv("data/age_gender_bkts.csv").drop(['year'], axis=1)  # all data is from 2015
+    getBucketStartInt = lambda x: int(re.search(r'\d+', x['age_bucket']).group())
+    buckets['age_start'] = buckets.apply(getBucketStartInt, axis=1)
+    return buckets
+
+
+def joinAgeGenderBucketFeatues(buckets, users):
+    # move relevant features alongside new features
+    ages = users.pop('age')
+    genders = users.pop('gender')
+    users['age'] = ages
+    users['gender'] = genders
+    users['language_copy'] = users['language']
+
+    BUCKET_SIZE = 5  # years
+    SUFFIX_1 = '_sameGender_population'
+    SUFFIX_2 = '_oppositeGender_population'
+
+    countries = sorted(buckets['country_destination'].unique())
+    for country in countries:
+        users[country + SUFFIX_1] = np.nan
+        users[country + SUFFIX_2] = np.nan
+
+    for _, bucket in buckets.iterrows():
+        ageStart = bucket['age_start']
+        ageEnd = ageStart + BUCKET_SIZE
+        population = bucket['population_in_thousands']
+        if bucket['gender'] == 'male':
+            users.loc[(ageStart <= users['age']) & (users['age'] <= ageEnd) & (users['gender'] == 'MALE'),
+                      bucket['country_destination'] + SUFFIX_1] = population
+            users.loc[(ageStart <= users['age']) & (users['age'] <= ageEnd) & (users['gender'] == 'FEMALE'),
+                      bucket['country_destination'] + SUFFIX_2] = population
+        elif bucket['gender'] == 'female':
+            users.loc[(ageStart <= users['age']) & (users['age'] <= ageEnd) & (users['gender'] == 'MALE'),
+                      bucket['country_destination'] + SUFFIX_2] = population
+            users.loc[(ageStart <= users['age']) & (users['age'] <= ageEnd) & (users['gender'] == 'FEMALE'),
+                      bucket['country_destination'] + SUFFIX_1] = population
+
+    return users
+
+
 start = datetime.now()
 
 train = pd.read_csv("data/train_users_2.csv").drop('date_first_booking', axis=1)
-train = normalizeData(train)
 test = pd.read_csv("data/test_users.csv").drop('date_first_booking', axis=1)
+print("Loaded train and test sets")
+
+ageGenderBuckets = getAgeGenderBuckets()
+train = joinAgeGenderBucketFeatues(ageGenderBuckets, train)
+test = joinAgeGenderBucketFeatues(ageGenderBuckets, test)
+print("Joined with age and gender features per country")
+
+train = normalizeData(train)
 test = normalizeData(test)
+print("Normalized numbers and labels")
 
 actions = pd.read_csv("data/user_actions_count.csv")
 tmp = len(actions.columns)
@@ -98,13 +151,13 @@ actions = dropLowCountColumns(actions, ACTION_COUNT_DROP_THRESHOLD)
 actions.set_index('user_id', inplace=True)
 print("Dropped %d actions due to rare use" % (tmp - len(actions.columns)))
 
-# usedDevice = getMostUsedDeviceByUserId()
-# elapsed = getSecsElapsedByUserId()
+usedDevice = getMostUsedDeviceByUserId()
+elapsed = getSecsElapsedByUserId()
 print("Processed session data")
 
 
 def joinTables(src):
-    return src.join(actions, how='left')#.join(usedDevice, how='left')#.join(elapsed, how='left')
+    return src.join(actions, how='left').join(usedDevice, how='left').join(elapsed, how='left')
 
 
 train = joinTables(train.set_index('user_id'))

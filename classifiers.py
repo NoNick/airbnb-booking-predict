18c9fed3cfaaa31433cda994092ed07f138cebe5
NoNick <aleksandrs634@gmail.com>
2019-02-03 16:00:05 +0300

    no need for empty rows workaround with proper optimization technique

--- classifiers.py
+++ classifiers.py
@@ -1,4 +1,3 @@
-import numpy as np
 from datetime import datetime
 from xgboost.sklearn import XGBClassifier
 
@@ -10,11 +9,11 @@ THREADS = 4
 # 'clfName': classifierObj
 _clfs = {
     'BaseFeatures': XGBClassifier(
-        # max_depth=6,
-        # learning_rate=0.02,
-        # n_estimators=300,
-        learning_rate=0.1,
-        n_estimators=60,
+        max_depth=6,
+        learning_rate=0.02,
+        n_estimators=300,
+        # learning_rate=0.1,
+        # n_estimators=60,
         subsample=0.5,
         colsample_bytree=0.15,
         missing=NA_CONST,
@@ -23,11 +22,11 @@ _clfs = {
         n_jobs=THREADS,
         n_threads=THREADS),
     'AgeGender': XGBClassifier(
-        # max_depth=7,
-        # learning_rate=0.02,
-        # n_estimators=350,
-        learning_rate=0.1,
-        n_estimators=70,
+        max_depth=7,
+        learning_rate=0.02,
+        n_estimators=350,
+        # learning_rate=0.1,
+        # n_estimators=70,
         subsample=0.5,
         colsample_bytree=0.4,
         missing=NA_CONST,
@@ -36,11 +35,11 @@ _clfs = {
         n_jobs=THREADS,
         n_threads=THREADS),
     'DAC': XGBClassifier(
-        # max_depth=1,
-        # learning_rate=0.03,
-        # n_estimators=150,
-        learning_rate=0.1,
-        n_estimators=45,
+        max_depth=1,
+        learning_rate=0.03,
+        n_estimators=150,
+        # learning_rate=0.1,
+        # n_estimators=45,
         subsample=0.5,
         colsample_bytree=0.1,
         missing=NA_CONST,
@@ -49,11 +48,11 @@ _clfs = {
         n_jobs=THREADS,
         n_threads=THREADS),
     'TFA': XGBClassifier(
-        # max_depth=1,
-        # learning_rate=0.03,
-        # n_estimators=150,
-        learning_rate=0.1,
-        n_estimators=45,
+        max_depth=1,
+        learning_rate=0.03,
+        n_estimators=150,
+        # learning_rate=0.1,
+        # n_estimators=45,
         subsample=0.5,
         colsample_bytree=0.1,
         missing=NA_CONST,
@@ -62,12 +61,12 @@ _clfs = {
         n_jobs=THREADS,
         n_threads=THREADS),
     'Actions': XGBClassifier(
-        # max_depth=6,
-        # learning_rate=0.02,
-        # n_estimators=200,
-        max_depth=4,
-        learning_rate=0.1,
-        n_estimators=50,
+        max_depth=6,
+        learning_rate=0.02,
+        n_estimators=300,
+        # max_depth=4,
+        # learning_rate=0.1,
+        # n_estimators=50,
         subsample=0.5,
         colsample_bytree=0.3,
         missing=NA_CONST,
@@ -91,13 +90,7 @@ def getClassifiersList(X):
     return _clfs
 
 
-def filterAllColumnsMatch(X, y, filterConst=NA_CONST):
-    retainIndicesBool = np.logical_not(X.eq(NA_CONST).all(axis=1).values)
-    return X[retainIndicesBool], y[retainIndicesBool]
-
-
 def getTrainTestValidPredictions(clfs, X_train, y_train, X_valid):
-    # predictions on the validation and test sets
     p_valid = []
 
     clfN = len(clfs)
@@ -106,14 +99,10 @@ def getTrainTestValidPredictions(clfs, X_train, y_train, X_valid):
         start = datetime.now()
         startPos = clf.beginColumn
         endPos = clf.endColumn
-        #     First run. Training on (X_train, y_train) and predicting on X_valid.
-        X_train_filtered, y_train_filtered = filterAllColumnsMatch(X_train.iloc[:, startPos:endPos], y_train)
-        clf.fit(X_train_filtered, y_train_filtered)
+        clf.fit(X_train.iloc[:, startPos:endPos], y_train)
 
         X_valid_featured = X_valid.iloc[:, startPos:endPos]
         yv = clf.predict_proba(X_valid_featured)
-        # some users have no actions data at all, return their action classifier's probabilities as 0
-        yv[X_valid_featured.eq(NA_CONST).all(axis=1), :] = 0
         p_valid.append(yv)
         i += 1
         print(("Trained %3d/%d classifiers, " + str(datetime.now() - start) + " last one") % (i, clfN))
@@ -128,8 +117,7 @@ def learn(clfs, X, y):
         start = datetime.now()
         startPos = clf.beginColumn
         endPos = clf.endColumn
-        X_filtered, y_filtered = filterAllColumnsMatch(X.iloc[:, startPos:endPos], y)
-        clf.fit(X_filtered, y_filtered)
+        clf.fit(X.iloc[:, startPos:endPos], y)
         i += 1
         print(("Trained %3d/%d classifiers, " + str(datetime.now() - start) + " last one") % (i, clfN))
     return clfs
@@ -143,8 +131,6 @@ def predict(clfs, X):
         startPos = clf.beginColumn
         endPos = clf.endColumn
         yf = clf.predict_proba(X.iloc[:, startPos:endPos])
-        # some users have no actions data at all, return their action classifier's probabilities as 0
-        yf[X.eq(NA_CONST).all(axis=1), :] = 0
         p.append(yf)
         i += 1
         print("Got predictions from %3d/%d classifier" % (i, clfN))
